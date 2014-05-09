6cc9f6adbc9a9be67299755cb4f07a07e8241945
arbuzovada <arbuzovada@gmail.com>
2014-05-09 11:11:24 +0400

    edges for E calc

--- gibbsIsing.m
+++ gibbsIsing.m
@@ -19,24 +19,22 @@ function [E, D, M, S] = gibbsIsing(H, J, betaAll, num_iter, connect_type)
     beta0 = length(betaAll);
     
     % initialization
-    net = get_neighbors(vS, hS, beta0, connect_type);
+    [net, edges] = get_neighbors(vS, hS, beta0, connect_type);
     % CHANGE TO RANDI! ALL ONES FOR SMOOTHNESS
-    S = repmat(2 * randi(2, [vS, hS]) - 3, [1, 1, beta0]); %ones([vS, hS, beta0]); 
+    S = ones([vS, hS, beta0]); %repmat(2 * randi(2, [vS, hS]) - 3, [1, 1, beta0]); 
     E_samples = zeros(fix(2 * num_iter / 3), beta0);
     mu_samples = zeros(fix(2 * num_iter / 3), beta0);
     t0 = num_iter - fix(2 * num_iter / 3);
     
-    big_r = rand([N, beta0, num_iter]);
-    save big_S_r.mat S big_r
     for t = 1 : num_iter
         for i = 1 : N
             % get new distribution
             % p_i = p(x_i = 1 | X_{\i})
             p_i = 1 ./ (1 + exp(-2 * betaAll .* (J * sum(S(net{i}), 1) + H(i))));
-            S([0 : beta0 - 1] * N + i) = 2 * (big_r(i, :, t) < p_i) - 1; %rand(1, beta0)
+            S([0 : beta0 - 1] * N + i) = 2 * (rand(1, beta0) < p_i) - 1;
         end
         if t > t0
-            E_samples(t - t0, :) = get_E(S, J, H, connect_type);
+            E_samples(t - t0, :) = get_E(S, J, H, edges);
             mu_samples(t - t0, :) = get_mu(S);
         end
     end
@@ -52,17 +50,11 @@ function res = get_mu(S)
     res = squeeze(sum(sum(S)) / (size(S, 1) * size(S, 2)));
 end
 
-function res = get_E(S, J, H, connect_type)
+function res = get_E(S, J, H, edges)
 % This function calculates E(X)
-    [~, ~, beta0] = size(S);
+    [vS, hS, beta0] = size(S);
     sum_H = squeeze(sum(sum(repmat(H, [1, 1, beta0]) .* S)))';
-    if connect_type == 4
-        sum_J = squeeze(sum(sum(S(1 : (end - 1), :, :) .* S(2 : end, :, :))) + ...
-            sum(sum(S(:, 1 : (end - 1), :) .* S(:, 2 : end, :))))';
-    else
-        sum_J = squeeze(sum(sum(S(1 : (end - 1), :, :) .* S(2 : end, :, :))) + ...
-            sum(sum(S(:, 1 : (end - 1), :) .* S(:, 2 : end, :))) + ...
-            sum(sum(S(1 : (end - 1), 1 : (end - 1), :) .* S(2 : end, 2 : end, :))))';
-    end
+    S = reshape(S, [vS * hS, beta0]);
+    sum_J = sum(S(edges(:, 1)) .* S(edges(:, 2)));
     res = -(J * sum_J + sum_H);
 end
